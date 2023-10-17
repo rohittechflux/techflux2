a49a196fa4673d1dfdbe510caaef0a6741fcea3d
rohittechflux <rohitb@techflux.in>
2023-10-17 12:43:50 +0530

    First commit

---
+++ src/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to My Website!</h1>
+            <p>
+                Hello there! I am ChatGPT, a friendly AI assistant here to help you. 
+                Whether you have questions, need advice, or just want to chat, I'm here for you.
+            </p>
+            <p>
+                In this example, I've created a simple React functional component to greet you. 
+                Feel free to modify and expand upon it to build your own amazing website!
+            </p>
+        </div>
+    );
+};
+
+export default Home;
